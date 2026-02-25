b6ab902c96c4e84a1374641815f65abe575fdbd9
anggavb <anggavb8@gmail.com>
2026-02-25 15:24:53 +0700

    submit minitask 3

---
+++ minitask-3.js
@@ -0,0 +1,16 @@
+
+const moneys = [1000, 2000, 5000, 10000, 50000];
+let change = 121000
+let changeUse = []
+
+let i = 1
+while (change > 0) {
+  if (change >= moneys[moneys.length - i]) {
+    change -= moneys[moneys.length - i];
+    changeUse.push(moneys[moneys.length - i]);
+  } else {
+    i++;
+  }
+}
+
+console.log(changeUse);
